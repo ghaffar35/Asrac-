59dfb3d29322ad2a9d8ca3234395c875a924fd5a
Yose <youness;chetoui@gmail.com>
2016-11-09 14:14:07 +0100

    add data of test in sql

---
+++ db/data.sql
@@ -0,0 +1,34 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 09, 2016 at 02:13 PM
+-- Server version: 5.7.16-0ubuntu0.16.04.1
+-- PHP Version: 7.0.8-0ubuntu0.16.04.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Asrac`
+--
+
+--
+-- Dumping data for table `Article`
+--
+
+INSERT INTO `Article` (`art_id`, `art_title`, `art_content`, `art_image`, `art_date`, `art_theme`) VALUES
+(1, 'Hello world', 'Id quem domesticarum, o aliqua pariatur ad anim iudicem sed laboris in illum \r\n		mandaremus aut excepteur. Singulis do appellat. Aute sed ita culpa nescius non \r\n			aliquip tamen sint laboris malis, laboris magna nisi ubi malis, ullamco velit \r\n				quae id enim. Anim illustriora mentitum cillum ingeniis, singulis te excepteur o \r\n					nulla fabulas nam velit dolor, qui ab sint voluptate. Laboris quem mentitum hic \r\n						ex quorum laborum transferrem ne est anim exercitation ex multos expetendis ne \r\n						incididunt. Ut summis admodum eruditionem.', 'image_test', '2016-11-08 00:00:00', 'Test'),
+(2, 'Salut', 'Id quem domesticarum, o aliqua pariatur ad anim iudicem sed laboris in illum mandaremus aut excepteur. Singulis do appellat. Aute sed ita culpa nescius non aliquip tamen sint laboris malis, laboris magna nisi ubi malis, ullamco velit quae id enim. Anim illustriora mentitum cillum ingeniis, singulis te excepteur o nulla fabulas nam velit dolor, qui ab sint voluptate. Laboris quem mentitum hic ex quorum laborum transferrem ne est anim exercitation ex multos expetendis ne incididunt. Ut summis admodum eruditionem. ', 'test2.jpg', '2016-11-09 00:00:00', 'test'),
+(3, 'Bonjour', ' Id quem domesticarum, o aliqua pariatur ad anim iudicem sed laboris in illum mandaremus aut excepteur. Singulis do appellat. Aute sed ita culpa nescius non aliquip tamen sint laboris malis, laboris magna nisi ubi malis, ullamco velit quae id enim. Anim illustriora mentitum cillum ingeniis, singulis te excepteur o nulla fabulas nam velit dolor, qui ab sint voluptate. Laboris quem mentitum hic ex quorum laborum transferrem ne est anim exercitation ex multos expetendis ne incididunt. Ut summis admodum eruditionem. ', 'test3.jpg', '2016-11-10 00:00:00', 'test\r\n');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
