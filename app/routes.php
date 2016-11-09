16f382eb8440279b3d2d1da0677c8028045bfab1
Yose <youness;chetoui@gmail.com>
2016-11-09 16:07:27 +0100

    add page login and admin

--- app/routes.php
+++ app/routes.php
@@ -9,7 +9,10 @@ $app->get('/actualite', "Asrac\Controller\HomeController::allArticles")->bind('a
 $app->match('/article/{id}', "Asrac\Controller\HomeController::articleAction")->bind('article');
 
 // Login form
-$app->get('/login', "cms\Controller\HomeController::loginAction")->bind('login');
+$app->get('/login', "Asrac\Controller\HomeController::loginAction")->bind('login');
+
+// Admin zone
+$app->get('/admin', "Asrac\Controller\AdminController::indexAction")->bind('admin');
 
 // Add a new article
-$app->match('/admin/article/add', "cms\Controller\AdminController::addArticleAction")->bind('admin_article_add');
+$app->match('/admin/article/add', "Asrac\Controller\AdminController::addArticleAction")->bind('admin_article_add');
--- src/DAO/UserDAO.php
+++ src/DAO/UserDAO.php
@@ -45,43 +45,43 @@ class UserDAO extends DAO implements UserProviderInterface {
     {
         return 'Asrac\Domain\User' === $class;
     }
-//
-//    // Creates a User object based on a DB row.
-//    protected function buildDomainObject($row) {
-//        $user = new User();
-//        $user->setId($row['usr_id']);
-//        $user->setUsername($row['usr_name']);
-//        $user->setPassword($row['usr_password']);
-//        $user->setSalt($row['usr_salt']);
-//        $user->setRole($row['usr_role']);
-//        return $user;
-//    }
-//
-//	// Saves a user into the database.
-//    public function save(User $user) {
-//        $userData = array(
-//            'usr_name' => $user->getUsername(),
-//            'usr_salt' => $user->getSalt(),
-//            'usr_password' => $user->getPassword(),
-//            'usr_role' => $user->getRole()
-//            );
-//
-//        if ($user->getId()) {
-//            // The user has already been saved : update it
-//            $this->getDb()->update('t_user', $userData, array('usr_id' => $user->getId()));
-//        } else {
-//            // The user has never been saved : insert it
-//            $this->getDb()->insert('t_user', $userData);
-//            // Get the id of the newly created user and set it on the entity.
-//            $id = $this->getDb()->lastInsertId();
-//            $user->setId($id);
-//        }
-//    }
-//
-//     // Removes a user from the database.
-//    public function delete($id) {
-//        // Delete the user
-//        $this->getDb()->delete('t_user', array('usr_id' => $id));
-//    }
+
+    // Creates a User object based on a DB row.
+    protected function buildDomainObject($row) {
+        $user = new User();
+        $user->setId($row['usr_id']);
+        $user->setUsername($row['usr_name']);
+        $user->setPassword($row['usr_password']);
+        $user->setSalt($row['usr_salt']);
+        $user->setRole($row['usr_role']);
+        return $user;
+    }
+
+	// Saves a user into the database.
+    public function save(User $user) {
+        $userData = array(
+            'usr_name' => $user->getUsername(),
+            'usr_salt' => $user->getSalt(),
+            'usr_password' => $user->getPassword(),
+            'usr_role' => $user->getRole()
+            );
+
+        if ($user->getId()) {
+            // The user has already been saved : update it
+            $this->getDb()->update('t_user', $userData, array('usr_id' => $user->getId()));
+        } else {
+            // The user has never been saved : insert it
+            $this->getDb()->insert('t_user', $userData);
+            // Get the id of the newly created user and set it on the entity.
+            $id = $this->getDb()->lastInsertId();
+            $user->setId($id);
+        }
+    }
+
+     // Removes a user from the database.
+    public function delete($id) {
+        // Delete the user
+        $this->getDb()->delete('t_user', array('usr_id' => $id));
+    }
 
 }
