af98224e88772e19d60e36e8046333f8f27adeab
Yose <youness;chetoui@gmail.com>
2016-11-09 10:39:51 +0100

    the debut of class UserDAO

---
+++ src/DAO/UserDAO.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Asrac\DAO;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Asrac\Domain\User;
+
+class UserDAO extends DAO implements UserProviderInterface {
