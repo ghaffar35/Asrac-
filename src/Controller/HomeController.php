7b555f1d9633c93e38698dc18b974497d48f612c
Yose <youness;chetoui@gmail.com>
2016-11-09 10:15:07 +0100

    change the index twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,11 +6,10 @@ use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 
 class HomeController {
-	//
-//    public function indexAction(Application $app) {
-//        $articles = $app['dao.article']->findAll();
-//        return $app['twig']->render('index.html.twig', array('articles' => $articles));
-//    }
+    public function indexAction(Application $app) {
+        $articles = $app['dao.article']->findAll();
+        return $app['twig']->render('index.html.twig', array('articles' => $articles));
+    }
 
 	public function allArticles(Application $app) {
         $articles = $app['dao.article']->findAll();
