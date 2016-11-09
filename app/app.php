5a3469404b399a7d374c451c1d0a614e9ce7dd57
Yose <youness;chetoui@gmail.com>
2016-11-09 14:31:28 +0100

    add the articletype

--- app/app.php
+++ app/app.php
@@ -79,10 +79,3 @@ $app['dao.article'] = $app->share(function ($app) {
 //    return $app['twig']->render('error.html.twig', array('message' => $message));
 //});
 
-// Register JSON data decoder for JSON requests
-//$app->before(function (Request $request) {
-//    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
-//        $data = json_decode($request->getContent(), true);
-//        $request->request->replace(is_array($data) ? $data : array());
-//    }
-//});
---
+++ src/Form/Type/ArticleType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Asrac\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', 'text')
+            ->add('content', 'textarea')
+			->add('image', 'file', array(
+                    'data_class' => null
+			))
+			->add('theme', 'text')
+			->add('dateArt','date', array(
+    			'widget' => 'choice'
+			));
+    }
+
+    public function getName()
+    {
+        return 'article';
+    }
+}
