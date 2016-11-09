b9df54014f0f11992f554d82b95bf77a34698a0d
Yose <youness;chetoui@gmail.com>
2016-11-09 16:27:15 +0100

    add function edit

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -40,4 +40,28 @@ class AdminController {
 				'title' => 'New article',
 				'articleForm' => $articleForm->createView()));
 	}
+
+	public function editArticleAction($id, Request $request, Application $app) {
+        $article = $app['dao.article']->find($id);
+        $articleForm = $app['form.factory']->create(new ArticleType(), $article);
+        $articleForm->handleRequest($request);
+		if ($request->isMethod('POST')) {
+			if ($articleForm->isValid()) {
+				$files = $request->files->get($articleForm->getName());
+				/* Make sure that Upload Directory is properly configured and writable */
+				$path = __DIR__.'/../../web/images/Articles/';
+				$filename = $files['image']->getClientOriginalName();
+				$files['image']->move($path,$filename);
+				$article->setImage($filename);
+				$message = 'File was successfully uploaded!';
+			}
+    	}
+        if ($articleForm->isSubmitted() && $articleForm->isValid()) {
+            $app['dao.article']->save($article);
+            $app['session']->getFlashBag()->add('success', 'The article was succesfully updated.');
+        }
+        return $app['twig']->render('article_form.html.twig', array(
+            'title' => 'Edit article',
+            'articleForm' => $articleForm->createView()));
+   }
 }
