a4ba1fc63b3eee0a15d0e28f9f360c4e65c38c9e
Yose <youness;chetoui@gmail.com>
2016-11-09 10:19:27 +0100

    function one article

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,10 @@ class HomeController {
 
 	public function allArticles(Application $app) {
         $articles = $app['dao.article']->findAll();
-        return $app['twig']->render('index.html.twig', array('articles' => $articles));
+        return $app['twig']->render('articles.html.twig', array('articles' => $articles));
     }
+
+	public function articleAction($id, Request $request, Application $app) {
+        $article = $app['dao.article']->find($id);
+		return $app['twig']->render('article.html.twig', array('article' => $article));
 }
