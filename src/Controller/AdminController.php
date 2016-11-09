448b546d0b72caef68a57457964bd8cadf04d39b
Yose <youness;chetoui@gmail.com>
2016-11-09 14:46:15 +0100

    function add article in admincontroller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,3 +8,29 @@ use cms\Domain\Article;
 use cms\Form\Type\ArticleType;
 
 class AdminController {
+	public function addArticleAction(Request $request, Application $app) {
+			$article = new Article();
+			$articleForm = $app['form.factory']->create(new ArticleType(), $article);
+			$articleForm->handleRequest($request);
+			if ($request->isMethod('POST')) {
+				if ($articleForm->isValid()) {
+					$image = $request->files->get($articleForm->getName());
+					/* Make sure that Upload Directory is properly configured and writable */
+					$path = __DIR__.'/../../web/images/Articles/';
+					$filename = $image['image']->getClientOriginalName();
+					$image['image']->move($path,$filename);
+					$article->setImage($filename);
+
+					$message = 'File was successfully uploaded!';
+
+				}
+			}
+			if ($articleForm->isSubmitted() && $articleForm->isValid()) {
+				$app['dao.article']->save($article);
+				$app['session']->getFlashBag()->add('success', 'The article was successfully created.');
+			}
+			return $app['twig']->render('article_form.html.twig', array(
+				'title' => 'New article',
+				'articleForm' => $articleForm->createView()));
+	}
+}
