eab0a3a08d14f41c7ed22e0d94419af5049ff043
Yose <youness;chetoui@gmail.com>
2016-11-09 14:36:32 +0100

    debut of admin controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,1 +1,10 @@
 <?php
+
+namespace Asrac\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use cms\Domain\Article;
+use cms\Form\Type\ArticleType;
+
+class AdminController {
