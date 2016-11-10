3b78b7ea5ac6d0441cb9a314c99a41b3e04ec3b4
Yose <youness;chetoui@gmail.com>
2016-11-10 15:16:45 +0100

    add function add user

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -72,5 +72,27 @@ class AdminController {
         // Redirect to admin home page
         return $app->redirect($app['url_generator']->generate('admin'));
     }
+
+	public function addUserAction(Request $request, Application $app) {
+        $user = new User();
+        $userForm = $app['form.factory']->create(new UserType(), $user);
+        $userForm->handleRequest($request);
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            // generate a random salt value
+            $salt = substr(md5(time()), 0, 23);
+            $user->setSalt($salt);
+            $plainPassword = $user->getPassword();
+            // find the default encoder
+            $encoder = $app['security.encoder.digest'];
+            // compute the encoded password
+            $password = $encoder->encodePassword($plainPassword, $user->getSalt());
+            $user->setPassword($password);
+            $app['dao.user']->save($user);
+            $app['session']->getFlashBag()->add('success', 'The user was successfully created.');
+        }
+        return $app['twig']->render('user_form.html.twig', array(
+            'title' => 'New user',
+            'userForm' => $userForm->createView()));
+    }
 }
 
