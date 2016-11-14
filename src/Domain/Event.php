1ac9e0036d874ad572adb1f1247213217820ae19
Yose <youness;chetoui@gmail.com>
2016-11-14 10:21:25 +0100

    add function set and get bin class event

--- src/Domain/Event.php
+++ src/Domain/Event.php
@@ -9,3 +9,59 @@ class Event {
     private $image;
     private $dateEv;
     private $desc;
+	
+	public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function getLieu() {
+        return $this->lieu;
+    }
+
+    public function setContent($lieu) {
+        $this->lieu = $lieu;
+    }
+
+	public function getImage() {
+        return $this->image;
+    }
+
+    public function setImage($image) {
+        $this->image = $image;
+    }
+	public function getDateEv() {
+
+		if(is_string($this->dateEv)){
+			$this->dateEv = \DateTime::createFromFormat('Y-m-d H:i:s', $this->dateEv);
+		}
+        return $this->dateEv;
+    }
+
+    public function setDateArt($dateEv) {
+		if(! is_string($dateEv)){
+			$dateEv = $dateEv->format('Y-m-d H:i:s');
+		}
+        $this->dateEv = $dateEv;
+    }
+
+	public function getDesc() {
+        return $this->desc;
+    }
+
+    public function setDesc($desc) {
+        $this->desc = $desc;
+    }
+
+}
