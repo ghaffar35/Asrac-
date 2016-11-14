2b6786b990e7e0374b7f6603b484db133655fddb
Yose <youness;chetoui@gmail.com>
2016-11-14 10:47:52 +0100

    add function find all for event

--- src/DAO/EventDAO.php
+++ src/DAO/EventDAO.php
@@ -5,3 +5,17 @@ namespace Asrac\DAO;
 use Asrac\Domain\Event;
 
 class EventDAO extends DAO {
+
+	// Return a list of all event, sorted by date (most recent first).
+    public function findAll() {
+        $sql = "select * from Event order by ev_id desc";
+        $result = $this->getDb()->fetchAll($sql);
+
+        // Convert query result to an array of domain objects
+        $events = array();
+        foreach ($result as $row) {
+            $eventId = $row['ev_id'];
+            $events[$eventId] = $this->buildDomainObject($row);
+        }
+        return $events;
+    }
