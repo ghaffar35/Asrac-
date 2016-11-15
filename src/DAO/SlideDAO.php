5a1401e47e60a7031153392b1752b0bc70bcbfe0
Yose <youness;chetoui@gmail.com>
2016-11-15 16:55:20 +0100

    function all slide

--- src/DAO/SlideDAO.php
+++ src/DAO/SlideDAO.php
@@ -6,4 +6,17 @@ use Asrac\Domain\Slide;
 
 class SlideDAO extends DAO {
 
+	// Return a list of all slide, sorted by id (most recent first).
+    public function findAll() {
+        $sql = "select * from Slide order by sl_id desc";
+        $result = $this->getDb()->fetchAll($sql);
+
+        // Convert query result to an array of domain objects
+        $slides = array();
+        foreach ($result as $row) {
+            $slidesId = $row['ev_id'];
+            $slides[$slidesId] = $this->buildDomainObject($row);
+        }
+        return $slides;
+    }
 }
