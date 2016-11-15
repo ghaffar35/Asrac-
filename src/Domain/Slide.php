ae4b51815e24851e87782033cb57dc1781a2c8e5
Yose <youness;chetoui@gmail.com>
2016-11-15 16:51:03 +0100

    add function set and get for class slide

--- src/Domain/Slide.php
+++ src/Domain/Slide.php
@@ -8,4 +8,36 @@ class Slide {
 	private $subTitle;
 	private $image;
 
+	public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function getSubTitle() {
+        return $this->subTitle;
+    }
+
+    public function setSubTitle($subTitle) {
+        $this->subTitle = $subTitle;
+    }
+
+	public function getImage() {
+        return $this->image;
+    }
+
+    public function setImage($image) {
+        $this->image = $image;
+    }
+
 }
