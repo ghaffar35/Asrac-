3ecf15a79e32e23f3e2acceb2b33912878d54be3
Yose <youness;chetoui@gmail.com>
2016-11-17 09:47:15 +0100

    clean slideDAO

--- src/DAO/SlideDAO.php
+++ src/DAO/SlideDAO.php
@@ -61,10 +61,4 @@ class SlideDAO extends DAO {
             $slide->setId($id);
         }
     }
-
-	// Removes an slide from the database.
-//    public function delete($id) {
-//        // Delete the slide
-//        $this->getDb()->delete('Slide', array('sl_id' => $id));
-//    }
 }
